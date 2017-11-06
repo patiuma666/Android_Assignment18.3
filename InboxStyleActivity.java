5455059f7a0f7af890225c01566f16cbc1d8e090
patiuma666 <patiuma666@gmail.com>
2017-11-06 15:40:57 +0530

    Add files via upload

---
+++ InboxStyleActivity.java
@@ -0,0 +1,13 @@
+ package com.example.iis5.inboxstylenotification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//created inboxStyleActivitty
+public class InboxStyleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inbox_style);
+    }
+}
